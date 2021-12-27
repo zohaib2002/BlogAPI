97e1fe93e75f26b8d15752ae18a0b819c81928b9
Zohaib Hamdule <zohaib2002@gmail.com>
2021-12-27 17:41:31 +0300

    Changed DB connection path

--- index.js
+++ index.js
@@ -1,156 +1,146 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
 // Import models
-const Post = require('./src/models/post')
+const Post = require("./src/models/post");
 
 // Define Application
-const app = express()
+const app = express();
 
 // Define Database Connection
-const db = mongoose.connect('mongodb://localhost:27017/blogdb')
+const db = mongoose.connect(
+  "mongodb+srv://blogapp:1234@blogapp.vngic.mongodb.net/BlogApp?retryWrites=true&w=majority"
+);
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-
-app.get('/', function(req, res) {
-    // Handle the request for the root route
-    res.send({ping: 'pong'})
-})
-
-
+app.get("/", function (req, res) {
+  // Handle the request for the root route
+  res.send({ ping: "pong" });
+});
 
 // Create a post
 // USAGE: POST http://localhost:8080/posts
 // Request body contains the post in JSON format
-app.post('/posts', function(req, res) {
+app.post("/posts", function (req, res) {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).send({ msg: "Please provide a body for the request" });
+  }
 
-    if (Object.keys(req.body).length === 0) {
-        res.status(400).send({msg: "Please provide a body for the request"})
+  // Get values from request payload
+  const title = req.body.title;
+  const author = req.body.author;
+  const content = req.body.content;
+  var post = new Post();
+
+  // Assign values to post model
+  post.title = title;
+  post.author = author;
+  post.content = content;
+  post.save(function (error, savedPost) {
+    if (error) {
+      // send error response
+      res.status(500).send({ error: "Unable to save Post" });
+    } else {
+      // send success response
+      res.status(200).send(savedPost);
     }
-
-    // Get values from request payload
-    const title = req.body.title
-    const author = req.body.author
-    const content = req.body.content
-    var post = new Post()
-
-    // Assign values to post model
-    post.title = title
-    post.author = author
-    post.content = content
-    post.save(function(error, savedPost) {
-        if (error) {
-            // send error response
-            res.status(500).send({ error: 'Unable to save Post' })
-        } else {
-            // send success response
-            res.status(200).send(savedPost)
-        }
-    })
-    //res.send({title: title, author: author, content: content})
-})
-
+  });
+  //res.send({title: title, author: author, content: content})
+});
 
 // Get a Post by its title
 // USAGE: GET http://localhost:8080/posts?title=Catchy+Blog+Title
 // Returns the list of all posts if no query passed
 // USAGE: GET http://localhost:8080/posts
-app.get('/posts', function(req, res) {
+app.get("/posts", function (req, res) {
+  queryPassed = !(Object.keys(req.query).length === 0);
 
-    queryPassed = !(Object.keys(req.query).length === 0)
+  var title = "";
 
-    var title = ""
+  if (queryPassed) {
+    title = req.query.title;
+    title = title.split("+").join(" ");
+  }
 
-    if (queryPassed) {
-        title = req.query.title
-        title = title.split('+').join(' ')
-    }
-
-    Post.find((queryPassed)?{title: title}:{}, function(error, posts) {
-      if(error) {
-        // send error response
-        res.status(422).send({ error: 'Unable to fetch posts '})
-      } else {
-        // send success response
-        // returns an empty list if no matchin posts
-        res.status(200).send(posts)
-      }
-    })
-  })
-
-  // Deleting a post
-  // USAGE: DELETE http://localhost:8080/posts?title=Catchy+Blog+Title
-  app.delete('/posts', function(req, res) {
-
-    queryPassed = !(Object.keys(req.query).length === 0)
-
-    var title = ""
-
-    if (queryPassed) {
-        title = req.query.title
-        title = title.split('+').join(' ')
+  Post.find(queryPassed ? { title: title } : {}, function (error, posts) {
+    if (error) {
+      // send error response
+      res.status(422).send({ error: "Unable to fetch posts " });
     } else {
-        res.status(400).send({error: "Must provide a title to delete"})
+      // send success response
+      // returns an empty list if no matchin posts
+      res.status(200).send(posts);
     }
+  });
+});
 
-    Post.find({title: title}, function(error, posts) {
-      if(error) {
-        // send error response
-        res.status(422).send({ error: 'Unable to fetch posts '})
-      } else {
-        // send success response
-        // we are expecting all titles to be unique
-        if (posts.length > 0) {
-            posts[0].delete()
-            res.status(200).send({msg: "Post deleted successfully"})
-        } else {
-            res.status(422).send({error: "No Post Deleted"})
-        }
-      }
-    })
-  })
+// Deleting a post
+// USAGE: DELETE http://localhost:8080/posts?title=Catchy+Blog+Title
+app.delete("/posts", function (req, res) {
+  queryPassed = !(Object.keys(req.query).length === 0);
 
+  var title = "";
 
-  // Updating a post
-  // USAGE: PATCH http://localhost:8080/posts?title=Catchy+Blog+Title
-  // The new post should be passed as request body in JSON format
-  app.patch('/posts', function(req, res) {
-    
-    if (Object.keys(req.body).length === 0) {
-        res.status(400).send({msg: "Please provide a body for the request"})
-    }
+  if (queryPassed) {
+    title = req.query.title;
+    title = title.split("+").join(" ");
+  } else {
+    res.status(400).send({ error: "Must provide a title to delete" });
+  }
 
-    var newPost = {
-        title: req.body.title,
-        author: req.body.author,
-        content: req.body.content
-    }    
-
-    queryPassed = !(Object.keys(req.query).length === 0)
-
-    var title = ""
-
-    if (queryPassed) {
-        title = req.query.title
-        title = title.split('+').join(' ')
+  Post.find({ title: title }, function (error, posts) {
+    if (error) {
+      // send error response
+      res.status(422).send({ error: "Unable to fetch posts " });
     } else {
-        res.status(400).send({error: "Must provide a title to modify"})
+      // send success response
+      // we are expecting all titles to be unique
+      if (posts.length > 0) {
+        posts[0].delete();
+        res.status(200).send({ msg: "Post deleted successfully" });
+      } else {
+        res.status(422).send({ error: "No Post Deleted" });
+      }
     }
+  });
+});
 
-   Post.updateOne({title: title}, newPost, function(err, docs) {
-        if (err){
-            res.status(400).send({error: 'Unable to modify records'})
-        }
-        else{
-           res.status(200).send(docs)
-        }
-   })
-  })
+// Updating a post
+// USAGE: PATCH http://localhost:8080/posts?title=Catchy+Blog+Title
+// The new post should be passed as request body in JSON format
+app.patch("/posts", function (req, res) {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).send({ msg: "Please provide a body for the request" });
+  }
 
+  var newPost = {
+    title: req.body.title,
+    author: req.body.author,
+    content: req.body.content,
+  };
 
+  queryPassed = !(Object.keys(req.query).length === 0);
 
-app.listen(8080, function() {
-    console.log('Server is running on PORT 8080')
-})
+  var title = "";
+
+  if (queryPassed) {
+    title = req.query.title;
+    title = title.split("+").join(" ");
+  } else {
+    res.status(400).send({ error: "Must provide a title to modify" });
+  }
+
+  Post.updateOne({ title: title }, newPost, function (err, docs) {
+    if (err) {
+      res.status(400).send({ error: "Unable to modify records" });
+    } else {
+      res.status(200).send(docs);
+    }
+  });
+});
+
+app.listen(8080, function () {
+  console.log("Server is running on PORT 8080");
+});
